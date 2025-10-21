1853fc0e6fe0b8352c432496e53ff337f4a7e64d
mahmoudtawfek22 <momohoho2224@gmail.com>
2025-10-21 06:09:33 +0300

    add production environment

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDYbbm-Wi2NSV0FOsX6VF4k3fz6SkJKgiU',
+    authDomain: 'let-s-chat-7a3fe.firebaseapp.com',
+    databaseURL: 'https://let-s-chat-7a3fe-default-rtdb.firebaseio.com',
+    projectId: 'let-s-chat-7a3fe',
+    storageBucket: 'let-s-chat-7a3fe.firebasestorage.app',
+    messagingSenderId: '57798030671',
+    appId: '1:57798030671:web:8ebd0b574ab5e945e4c13a',
+    measurementId: 'G-6RZ5WXK498',
+  },
+};
